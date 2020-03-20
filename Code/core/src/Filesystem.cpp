32b5d6b3edaaf957880905dd98316bb3d310ff05
Maxime Griot <maxime@griot.fr>
2020-03-20 15:01:48 +0100

    Filesystem helpers

---
+++ Code/core/src/Filesystem.cpp
@@ -0,0 +1,39 @@
+#include "Filesystem.hpp"
+
+#include <fstream>
+
+#if TP_PLATFORM_WINDOWS
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#endif
+
+namespace TiltedPhoques
+{
+    std::filesystem::path GetPath() noexcept
+    {
+#if TP_PLATFORM_WINDOWS
+        WCHAR dllPath[MAX_PATH] = { 0 };
+        GetModuleFileNameW(reinterpret_cast<HINSTANCE>(&__ImageBase), dllPath, std::size(dllPath));
+
+        std::error_code ec;
+        const auto currentPath = std::filesystem::path(dllPath).parent_path();
+
+        return currentPath;
+#else
+        return std::filesystem::current_path();
+#endif
+    }
+
+    String LoadFile(const std::filesystem::path& acPath) noexcept
+    {
+        std::ifstream file(acPath, std::ios::binary);
+        file.seekg(0, std::ifstream::end);
+        const size_t length = file.tellg();
+        file.seekg(0, std::ifstream::beg);
+
+        String content(length, '\0');
+        file.read(content.data(), length);
+
+        return content;
+    }
+
+}
